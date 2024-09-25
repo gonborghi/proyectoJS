79171752d50666937fb0c7576c658c19060412b0
gonzalo <gonzaloborghi05@gmail.com>
2024-09-25 20:40:58 -0300

    Tercer preentrega

--- js/main.js
+++ js/main.js
@@ -1,63 +1,63 @@
-
 const messi = {
     nombre: "Messi", 
     edad: 37,
-}
+};
 
-const edadesCercanas = [36, 38, 35, 39]
-let intentosFallidos = []
-let puntos = 100
+const edadesCercanas = [36, 38, 35, 39];
+let intentosFallidos = [];
+let puntos = 100;
+const respuestasEnviadas = [];
+let intentos = 0;
 
+const formulario = document.getElementById('formulario');
+const mensajeResultado = document.getElementById('mensajeResultado')
 
-let respuestaUser
-let intentos = 0
-
-function pregunta(respuestaUser){
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+    agregarRespuestas();
+    contadorPuntos;
     
-    while (respuestaUser != messi.edad) {
-        while (puntos > 0){
-        respuestaUser = parseInt(prompt("¿Cuál es la edad de Lionel Messi?"))
-        
-         if (edadesCercanas.includes(respuestaUser)) {
-            puntos -= 15
-            alert("Estás muy cerca!. Perdiste 15 puntos. Tus puntos son: " + puntos )
-            intentosFallidos.push(respuestaUser)
-            intentos ++
-            
-            
-        
-        } else if (isNaN(respuestaUser)){
-            alert("El dato ingresado no es una edad.")
-            
+});
 
-        } else if (respuestaUser == messi.edad){
-
-            intentos ++
-            alert("Correcto! Lo adivinaste en " + intentos + " intentos. " + " Obtuviste " + puntos + " puntos")
-
-            if (intentos != 1){
-                alert("Los números incorrectos que ingresaste fueron: " + intentosFallidos)
-            }
-            if (intentos > 2 ){
-                alert(" La edad más grande que ingresaste fue " + Math.max(...intentosFallidos) + " y la más chica " + Math.min(...intentosFallidos))
-            }  
-
-        }  else {
-            puntos -= 30
-            alert("Seguí intentando. Perdiste 30 puntos. Tus puntos son: " + puntos)
-            intentosFallidos.push(respuestaUser)
-            intentos ++
-            
-            
-        }   
-        }
-        
-        alert("Perdiste")
-        break
-    }
-
-    return
+function agregarRespuestas() {
+    const edad = parseInt(document.getElementById('edad').value); 
+   
+    respuestasEnviadas.push(edad);
+    localStorage.setItem('respuestas', JSON.stringify(respuestasEnviadas));
+    
+    contadorPuntos(edad); 
+    
+    formulario.reset();
 }
 
-pregunta()
+const respuestas = document.getElementById('respuestas');
+const verRespuestas = document.getElementById('verRespuestas');
 
+verRespuestas.addEventListener('click', () => {
+    mostrarRespuestas();
+});
+
+function mostrarRespuestas() {
+    respuestas.innerHTML = '';
+    const respuestasGuardadas = JSON.parse(localStorage.getItem('respuestas')) || [];
+    respuestasGuardadas.forEach((res) => {
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <div>
+                <p>Edad: ${res}</p>
+                <p>Puntos: ${puntos}</p>
+            </div>
+        `;
+        respuestas.appendChild(div);
+    });
+}
+
+function contadorPuntos(edad) {
+    if (edad !== messi.edad) {  
+        puntos -= 10;
+        mensajeResultado.textContent = "La respuesta es incorrecta, perdiste 10 puntos"; 
+    }
+    else{
+        mensajeResultado.textContent = "La respuesta es correcta"; 
+    } 
+}
